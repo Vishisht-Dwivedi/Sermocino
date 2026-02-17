5029df91f898ecd3bfcf5d0b9976d91ef6af45f6
Vishisht Dwivedi <170785484+Vishisht-Dwivedi@users.noreply.github.com>
2026-02-17 17:13:57 +0530

    minor: restructured and broke route into schema+route

---
+++ services/auth-service/src/schema/register.schema.ts
@@ -0,0 +1,98 @@
+import { RouteShorthandOptions } from "fastify";
+export const registerSchema: RouteShorthandOptions = {
+  schema: {
+    tags: ["Auth"],
+    summary: "Register a new user",
+
+    body: {
+      type: "object",
+      required: ["email", "username", "password"],
+      properties: {
+        email: {
+          type: "string",
+          format: "email"
+        },
+        username: {
+          type: "string",
+          minLength: 3,
+          maxLength: 100
+        },
+        password: {
+          type: "string",
+          minLength: 6
+        }
+      }
+    },
+    response: {
+      201: {
+        type: "object",
+        required: ["status", "data"],
+        properties: {
+          status: { 
+            type: "string",
+            enum: ["ok"]
+          },
+          data: {
+            type: "object",
+            required: ["username", "email"],
+            properties: {
+              username: { type: "string" },
+              email: { type: "string" }
+            }
+          }
+         }
+      },
+      400: {
+        type: "object",
+        required: ["status", "data"],
+        properties: {
+          status: { 
+            type: "string",
+            enum: ["error"]
+          },
+          data: {
+            type: "object",
+            required: ["type", "caused_by"],
+            properties: {
+              type: { type: "string" },
+              caused_by: { type: "string" }
+            }
+          }
+        }
+      },
+      409: {
+        type: "object",
+        required: ["status", "data"],
+        properties: {
+          status: { 
+            type: "string",
+            enum: ["error"]
+          },
+          data: {
+            type: "object",
+            required: ["type", "field"],
+            properties: {
+              type: { type: "string", enum: ["duplicate"] },
+              field: { type: "string", enum: ["email", "username"] }
+            }
+          }
+        }
+      },
+      500: {
+        type: "object",
+        properties: {
+          status: {
+            type: "string",
+            enum: ["error"]
+          },
+          body: {
+            type: "object",
+            properties: {
+              message: { type:"string" }
+            }
+          }
+        }
+      }
+    }
+  }
+};
